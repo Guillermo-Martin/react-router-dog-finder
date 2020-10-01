615dc745b9b73f20bf432ff49a787494d9936e3c
gcmartin <gscalica@gmail.com>
2020-10-01 12:03:48 -0700

    Create Card component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Card from './components/Card';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <h1>React Router Dog Finder</h1>
       <Home />
+      <Card />
     </div>
   );
 }
