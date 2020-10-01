83884f770db5f0ccd54b7c237a5af81c8e06fdc1
gcmartin <gscalica@gmail.com>
2020-10-01 11:29:37 -0700

    Create 'components' folder and create Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>React Router Dog Finder</h1>
       <h2>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
