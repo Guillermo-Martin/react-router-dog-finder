8459a7acfe1d3e872e4d9aab9663a016eabe02bf
gcmartin <gscalica@gmail.com>
2020-10-01 13:44:19 -0700

    Convert App component to class component, move defaultProps and methods map functions from Home to App

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,56 +1,31 @@
 import React, { Component } from 'react';
 import Card from '../components/Card';
+import Navbar from '../components/Navbar';
 
 class Home extends Component {
-  static defaultProps = {
-    dogs: [
-      {
-        name: "Whiskey",
-        age: 5,
-        // src: whiskey,
-        facts: [
-          "Whiskey loves eating popcorn.",
-          "Whiskey is a terrible guard dog.",
-          "Whiskey wants to cuddle with you!"
-        ]
-      },
-      {
-        name: "Hazel",
-        age: 3,
-        // src: hazel,
-        facts: [
-          "Hazel has soooo much energy!",
-          "Hazel is highly intelligent.",
-          "Hazel loves people more than dogs."
-        ]
-      },
-      {
-        name: "Tubby",
-        age: 4,
-        // src: tubby,
-        facts: [
-          "Tubby is not the brightest dog",
-          "Tubby does not like walks or exercise.",
-          "Tubby loves eating food."
-        ]
-      }
-    ]
-  }
-  
+
   render() {
     // render a card for each dog in the array
-    const dogsArr = this.props.dogs.map(dog => 
-      <Card 
+    const dogsArr = this.props.dogs.map(dog =>
+      <Card
         name={dog.name}
         age={dog.age}
         facts={dog.facts}
       />
     );
 
-    return(
+    // render a <li> for every dog in the array and pass to Navbar
+    const navLinksArr = this.props.dogs.map(dog =>
+      <li>{dog.name}</li>
+    )
+
+    return (
       <div>
-      <p>{dogsArr}</p>
-    </div>
+        <Navbar
+          links={navLinksArr}
+        />
+        <p>{dogsArr}</p>
+      </div>
     )
   }
 }
