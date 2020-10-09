e725b02886a499b87e1efe2735d6713693aa6c07
gcmartin <gscalica@gmail.com>
2020-10-09 13:57:32 -0700

    Add map function to create array to render dog image and name

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,9 +8,18 @@ class Home extends Component {
 
     const allDogs = this.props.dogs;
 
+    // for every dog in the allDogs array render and image with a name
+    const dogImgs = allDogs.map(dog => 
+        <div key={dog.name}>
+          <img src={dog.src} alt={dog.name} />
+          <p>{dog.name}</p>
+        </div>
+      )
+
     return (
       <div>
         <h1>This is the home page</h1>
+        {dogImgs}
       </div>
     );
   }
